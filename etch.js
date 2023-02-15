7d852c92780fc64b2e89b7d1852f178558faf7c4
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-15 23:25:53 +0800

    Feat: create 16x16 divs, add class hover on mouseover

--- etch.js
+++ etch.js
@@ -1,9 +1,17 @@
 const container = document.querySelector('#containerGrid');
 const dimension = 16; //16x16 grid of square divs
 
-for(i=0;i<16;i++) {
+for(i=0;i<256;i++) {
     const miniGrid = document.createElement('div');
-    miniGrid.classList.add(`grid${i}`);
-    miniGrid.textContent = `${i}`;
+    miniGrid.classList.add(`grid`);
+    //miniGrid.textContent = `${i}`;
     container.appendChild(miniGrid);    
 }
+
+const grids = document.querySelectorAll('.grid');
+
+grids.forEach(grid => {
+    grid.addEventListener('mouseover', (e) => {
+        grid.classList.add('hover');
+    });
+});
