75715033d2fafe8fca1082a5e3605543147c85b4
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-17 11:01:10 +0800

    Feat: allow reset of grid with user input dimensions

--- etch.js
+++ etch.js
@@ -1,10 +1,11 @@
 const container = document.querySelector('#containerGrid');
-const dimension = 16; //16x16 grid of square divs
+let dimension = 16; //16x16 grid of square divs
 
-for(i=0;i<256;i++) {
+for(i=0;i<dimension*dimension;i++) {
     const miniGrid = document.createElement('div');
     miniGrid.classList.add(`grid`);
-    //miniGrid.textContent = `${i}`;
+    miniGrid.style.width = `${300/dimension}px`;
+    miniGrid.style.height = `${300/dimension}px`;
     container.appendChild(miniGrid);    
 }
 
@@ -15,3 +16,31 @@ grids.forEach(grid => {
         grid.classList.add('hover');
     });
 });
+
+function resetGrid(e) {
+    dimension = prompt("enter new grid size (side)")
+    if(dimension > 100 || dimension < 1) {
+        dimension = prompt('Dimension must be > 0 and < 100, please enter again');
+    }
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for(i=0;i<dimension*dimension;i++) {
+        const miniGrid = document.createElement('div');
+        miniGrid.classList.add(`grid`);
+        miniGrid.style.width = `${300/dimension}px`;
+        miniGrid.style.height = `${300/dimension}px`;
+        container.appendChild(miniGrid);    
+    }
+    const grids = document.querySelectorAll('.grid');
+
+    grids.forEach(grid => {
+        grid.addEventListener('mouseover', (e) => {
+            grid.classList.add('hover');
+        });
+    });
+        
+}
+
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', resetGrid);
